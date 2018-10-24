b3767a79233a4f178874882bf54b2500dbb86098
Rares Iova <iovarares@gmail.com>
2018-10-24 18:23:57 +0300

    [Login]Fixed SecurityUtils

--- src/main/java/io/github/cristaling/swegg/backend/utils/SecurityUtils.java
+++ src/main/java/io/github/cristaling/swegg/backend/utils/SecurityUtils.java
@@ -1,43 +1,21 @@
 package io.github.cristaling.swegg.backend.utils;
 
+import java.util.Base64;
+
 public class SecurityUtils {
 
 	private static String API_TOKEN_SECRET = "Aseara ti-am luat basma";
 
 	//TODO Implement
 	public static String getTokenByUUID(String uuid) {
-		StringBuilder stringBuilder = new StringBuilder();
-
-		for (int i = 0; i < Math.max(API_TOKEN_SECRET.length(), uuid.length()); i++) {
-
-			int secretIndex = i % API_TOKEN_SECRET.length();
-			int uuidIndex = i % uuid.length();
-
-			char secretChar = API_TOKEN_SECRET.charAt(secretIndex);
-			char uuidChar = uuid.charAt(uuidIndex);
-			char resultChar = (char) (secretChar + uuidChar);
-
-			stringBuilder.append(resultChar);
-		}
-		return stringBuilder.toString();
+		String resultBeforeEncode = uuid + API_TOKEN_SECRET;
+		return Base64.getEncoder().encodeToString(resultBeforeEncode.getBytes());
 	}
 
 	//TODO Implement
 	public static String getUUIDByToken(String token) {
-		StringBuilder stringBuilder = new StringBuilder();
-
-		for (int i = 0; i < Math.max(API_TOKEN_SECRET.length(), token.length()); i++) {
-
-			int secretIndex = i % API_TOKEN_SECRET.length();
-			int tokenIndex = i % token.length();
-
-			char secretChar = API_TOKEN_SECRET.charAt(secretIndex);
-			char tokenChar = token.charAt(tokenIndex);
-			char resultChar = (char) (tokenChar - secretChar);
-
-			stringBuilder.append(resultChar);
-		}
-		return stringBuilder.toString();
+		String resultAfterDecode = new String(Base64.getDecoder().decode(token));
+		return resultAfterDecode.replace(API_TOKEN_SECRET, "");
 	}
 
 }
