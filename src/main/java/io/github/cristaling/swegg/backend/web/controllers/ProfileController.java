24f52c9a7fd91970835a1c6ca2c3ea5b2fcf6541
gradi <gradinariu.george97@gmail.com>
2018-10-25 21:02:23 +0300

    changed

--- src/main/java/io/github/cristaling/swegg/backend/web/controllers/ProfileController.java
+++ src/main/java/io/github/cristaling/swegg/backend/web/controllers/ProfileController.java
@@ -1,7 +1,6 @@
 package io.github.cristaling.swegg.backend.web.controllers;
 
 import io.github.cristaling.swegg.backend.service.ProfileService;
-import io.github.cristaling.swegg.backend.service.SecurityService;
 import io.github.cristaling.swegg.backend.web.responses.ProfileResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -10,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin
 @RestController
-@RequestMapping("/api/profile")
+@RequestMapping("/api/user")
 public class ProfileController {
 
     private ProfileService profileService;
@@ -25,7 +24,7 @@ public class ProfileController {
      * @return ProfileController -> all data needed for a profile status 200
      * @return returns status 404 if there are no user with this email
      */
-    @GetMapping("/get")
+    @GetMapping("/profile")
     public ResponseEntity getProfile(String email, @RequestHeader("Authorization") String token) {
         ProfileResponse profileResponse= profileService.getProfile(email,token);
         if(profileResponse!=null)
