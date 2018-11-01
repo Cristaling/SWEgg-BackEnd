3cd692ca5b968c9987c8ec1ab9ee4904cb3b4670
Andreea Ionut <andreea.ionut98@yahoo.com>
2018-11-01 16:45:38 +0200

    [Core]Created MemberReview entity

---
+++ src/main/java/io/github/cristaling/swegg/backend/core/member/MemberReview.java
@@ -0,0 +1,55 @@
+package io.github.cristaling.swegg.backend.core.member;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.Date;
+import java.util.UUID;
+
+@Entity
+@Table(name = "member_reviews")
+public class MemberReview {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    private UUID uuid;
+
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL, optional = false)
+    private Member reviewer;
+
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL, optional = false)
+    private Member reviewed;
+
+    @NotNull
+    private String text;
+    @NotNull
+    private int stars;
+    @NotNull
+    private Date dateGiven;
+
+    public UUID getUuid() { return uuid; }
+
+    public void setUuid(UUID uuid) { this.uuid = uuid; }
+
+    public Member getReviewer() { return reviewer; }
+
+    public void setReviewer(Member reviewer) { this.reviewer = reviewer; }
+
+    public Member getReviewed() { return reviewed; }
+
+    public void setReviewed(Member reviewed) { this.reviewed = reviewed; }
+
+    public String getText() { return text; }
+
+    public void setText(String text) { this.text = text; }
+
+    public int getStars() { return stars; }
+
+    public void setStars(int stars) { this.stars = stars; }
+
+    public Date getDateGiven() { return dateGiven; }
+
+    public void setDateGiven(Date dateGiven) { this.dateGiven = dateGiven; }
+}
