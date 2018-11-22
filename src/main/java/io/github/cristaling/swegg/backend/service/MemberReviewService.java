6e0381e35e5f42b2b97a0489db94dfd044c67a24
Andreea Ionut <andreea.ionut98@yahoo.com>
2018-11-22 22:45:16 +0200

    [MemberReview] Validations for add review / Added field Employee on Job entity

--- src/main/java/io/github/cristaling/swegg/backend/service/MemberReviewService.java
+++ src/main/java/io/github/cristaling/swegg/backend/service/MemberReviewService.java
@@ -1,9 +1,12 @@
 package io.github.cristaling.swegg.backend.service;
 
+import io.github.cristaling.swegg.backend.core.job.Job;
 import io.github.cristaling.swegg.backend.core.member.Member;
 import io.github.cristaling.swegg.backend.core.member.MemberReview;
+import io.github.cristaling.swegg.backend.repositories.JobRepository;
 import io.github.cristaling.swegg.backend.repositories.MemberReviewRepository;
 import io.github.cristaling.swegg.backend.repositories.UserRepository;
+import io.github.cristaling.swegg.backend.utils.enums.JobStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,16 +17,30 @@ public class MemberReviewService {
 
     private MemberReviewRepository memberReviewRepository;
     private UserRepository userRepository;
+    private JobRepository jobRepository;
 
     @Autowired
-    public MemberReviewService(MemberReviewRepository memberReviewRepository, UserRepository userRepository) {
+    public MemberReviewService(MemberReviewRepository memberReviewRepository, UserRepository userRepository, JobRepository jobRepository) {
         this.memberReviewRepository = memberReviewRepository;
         this.userRepository = userRepository;
+        this.jobRepository = jobRepository;
     }
 
     public MemberReview addMemberReview(Member reviewer, String reviewedEmail, String text, int stars){
+        Member reviewed = this.userRepository.getMemberByEmail(reviewedEmail);
+        if(reviewed == null){
+            return null;
+        }
+        Job job = this.jobRepository.getJobByOwnerAndEmployee(reviewer, reviewed);
+        if(job == null){
+            return null;
+        }
+        if(!job.getJobStatus().equals(JobStatus.DONE)){
+            return null;
+        }
+
         MemberReview memberReview = new MemberReview();
-        memberReview.setReviewed(this.userRepository.getMemberByEmail(reviewedEmail));
+        memberReview.setReviewed(reviewed);
         memberReview.setReviewer(reviewer);
         memberReview.setText(text);
         memberReview.setStars(stars);
