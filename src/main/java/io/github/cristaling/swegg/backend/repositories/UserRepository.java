1dd96b1965ba27dc8019c5fc2db52735c634bbb2
alexandruivv <alexandruivv@yahoo.com>
2018-12-17 00:06:48 +0200

    [Search-Users] Search user by name

--- src/main/java/io/github/cristaling/swegg/backend/repositories/UserRepository.java
+++ src/main/java/io/github/cristaling/swegg/backend/repositories/UserRepository.java
@@ -1,7 +1,11 @@
 package io.github.cristaling.swegg.backend.repositories;
 
 import io.github.cristaling.swegg.backend.core.member.Member;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
@@ -12,5 +16,10 @@ public interface UserRepository extends JpaRepository<Member, UUID> {
     Member getMemberByEmailAndPassword(String email, String password);
     Member getMemberByEmail(String email);
     Member getMemberByGoogleID(String googleID);
+
+    @Query("SELECT m FROM Member m WHERE " +
+            "concat(lower(m.memberData.firstName), ' ', lower(m.memberData.lastName)) LIKE %:name% or " +
+            "concat(lower(m.memberData.lastName), ' ', lower(m.memberData.firstName)) LIKE %:name% ")
+    Page<Member> getMemberByCompleteNameIgnoreCase(Pageable pageRequest, @Param("name") String name);
 
 }
