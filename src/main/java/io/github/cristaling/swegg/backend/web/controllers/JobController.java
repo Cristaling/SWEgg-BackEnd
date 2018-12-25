966ddb77efc0572142d21b9cae6a5b86e65716c5
Rares Iova <iovarares@gmail.com>
2018-12-25 22:30:36 +0200

    [Jobs]Added endpoint for top 5 most relevant jobs

--- src/main/java/io/github/cristaling/swegg/backend/web/controllers/JobController.java
+++ src/main/java/io/github/cristaling/swegg/backend/web/controllers/JobController.java
@@ -1,8 +1,10 @@
 package io.github.cristaling.swegg.backend.web.controllers;
 
+import io.github.cristaling.swegg.backend.core.abilities.Ability;
 import io.github.cristaling.swegg.backend.core.job.Job;
-import io.github.cristaling.swegg.backend.core.member.Member;
 import io.github.cristaling.swegg.backend.core.job.JobSummary;
+import io.github.cristaling.swegg.backend.core.member.Member;
+import io.github.cristaling.swegg.backend.service.AbilityService;
 import io.github.cristaling.swegg.backend.service.JobService;
 import io.github.cristaling.swegg.backend.service.SecurityService;
 import io.github.cristaling.swegg.backend.utils.enums.JobType;
@@ -29,81 +31,98 @@ import java.util.UUID;
 @RequestMapping("/api/job")
 public class JobController {
 
-    private JobService jobService;
+	private JobService jobService;
+	private AbilityService abilityService;
 
-    private SecurityService securityService;
+	private SecurityService securityService;
 
-    @Autowired
-    public JobController(JobService jobService, SecurityService securityService) {
-        this.jobService = jobService;
-        this.securityService = securityService;
-    }
+	@Autowired
+	public JobController(JobService jobService, AbilityService abilityService, SecurityService securityService) {
+		this.jobService = jobService;
+		this.abilityService = abilityService;
+		this.securityService = securityService;
+	}
 
-    @PostMapping
-    public ResponseEntity addJob(@RequestHeader("Authorization") String token, @RequestBody JobAddRequest jobAddRequest) {
+	@PostMapping
+	public ResponseEntity addJob(@RequestHeader("Authorization") String token, @RequestBody JobAddRequest jobAddRequest) {
 
-        if (!securityService.canAccessRole(token, MemberRole.CLIENT)) {
-            return new ResponseEntity(HttpStatus.UNAUTHORIZED);
-        }
-        Member userByToken = securityService.getUserByToken(token);
+		if (!securityService.canAccessRole(token, MemberRole.CLIENT)) {
+			return new ResponseEntity(HttpStatus.UNAUTHORIZED);
+		}
+		Member userByToken = securityService.getUserByToken(token);
 
-        JobWithAbilities job = jobService.addJob(jobAddRequest, userByToken);
+		JobWithAbilities job = jobService.addJob(jobAddRequest, userByToken);
 
-        if (job == null) {
-            return new ResponseEntity(HttpStatus.BAD_REQUEST);
-        }
-        return new ResponseEntity(HttpStatus.CREATED);
-    }
+		if (job == null) {
+			return new ResponseEntity(HttpStatus.BAD_REQUEST);
+		}
+		return new ResponseEntity(HttpStatus.CREATED);
+	}
 
-    @GetMapping("/summaries")
-    public ResponseEntity getJobSummaries(@RequestHeader("Authorization") String token,
-                                          @RequestParam(defaultValue = "") String title,
-                                          @RequestParam(defaultValue = "0") int page,
-                                          @RequestParam(defaultValue = "10") int count) {
+	@GetMapping("/summaries")
+	public ResponseEntity getJobSummaries(@RequestHeader("Authorization") String token,
+	                                      @RequestParam(defaultValue = "") String title,
+	                                      @RequestParam(defaultValue = "0") int page,
+	                                      @RequestParam(defaultValue = "10") int count) {
 
-        if (!securityService.canAccessRole(token, MemberRole.CLIENT)) {
-            return new ResponseEntity(HttpStatus.UNAUTHORIZED);
-        }
+		if (!securityService.canAccessRole(token, MemberRole.CLIENT)) {
+			return new ResponseEntity(HttpStatus.UNAUTHORIZED);
+		}
 
-        List<JobSummary> summaries = this.jobService.getJobSummaries(title, page, count);
+		List<JobSummary> summaries = this.jobService.getJobSummaries(title, page, count);
 
-        return new ResponseEntity(summaries, HttpStatus.OK);
-    }
+		return new ResponseEntity(summaries, HttpStatus.OK);
+	}
 
-    @GetMapping()
-    public ResponseEntity getJob(@RequestHeader("Authorization") String token, String jobUUID) {
-        if (!securityService.canAccessRole(token, MemberRole.CLIENT)) {
-            return new ResponseEntity(HttpStatus.UNAUTHORIZED);
-        }
+	@GetMapping()
+	public ResponseEntity getJob(@RequestHeader("Authorization") String token, String jobUUID) {
+		if (!securityService.canAccessRole(token, MemberRole.CLIENT)) {
+			return new ResponseEntity(HttpStatus.UNAUTHORIZED);
+		}
 
-        UUID uuid = UUID.fromString(jobUUID);
-        JobWithAbilities job = this.jobService.getJob(uuid);
+		UUID uuid = UUID.fromString(jobUUID);
+		JobWithAbilities job = this.jobService.getJob(uuid);
 
-        if (job == null) {
-            return new ResponseEntity(HttpStatus.BAD_REQUEST);
-        }
+		if (job == null) {
+			return new ResponseEntity(HttpStatus.BAD_REQUEST);
+		}
 
-        return new ResponseEntity(job, HttpStatus.OK);
-    }
+		return new ResponseEntity(job, HttpStatus.OK);
+	}
 
-    @GetMapping("/types")
-    public ResponseEntity getJobTypes(){
-        return new ResponseEntity(JobType.values(), HttpStatus.OK);
-    }
+	@GetMapping("/types")
+	public ResponseEntity getJobTypes() {
+		return new ResponseEntity(JobType.values(), HttpStatus.OK);
+	}
 
 	@GetMapping("/related")
-	public ResponseEntity getRelatedJobs(@RequestHeader("Authorization") String token,@RequestParam("email") String email) {
-        if (!securityService.canAccessRole(token, MemberRole.CLIENT)) {
-            return new ResponseEntity(HttpStatus.UNAUTHORIZED);
-        }
-        Member userByToken =securityService.getUserByToken(token);
-        if (userByToken == null) {
-            return new ResponseEntity(HttpStatus.UNAUTHORIZED);
-        }
-        List<JobSummary> jobSummaryList= jobService.getUserJobs(email,userByToken);
-        if(jobSummaryList==null){
-            return new ResponseEntity(HttpStatus.UNAUTHORIZED);
-        }
-        return new ResponseEntity(jobSummaryList,HttpStatus.OK);
+	public ResponseEntity getRelatedJobs(@RequestHeader("Authorization") String token, @RequestParam("email") String email) {
+		if (!securityService.canAccessRole(token, MemberRole.CLIENT)) {
+			return new ResponseEntity(HttpStatus.UNAUTHORIZED);
+		}
+		Member userByToken = securityService.getUserByToken(token);
+		if (userByToken == null) {
+			return new ResponseEntity(HttpStatus.UNAUTHORIZED);
+		}
+		List<JobSummary> jobSummaryList = jobService.getUserJobs(email, userByToken);
+		if (jobSummaryList == null) {
+			return new ResponseEntity(HttpStatus.UNAUTHORIZED);
+		}
+		return new ResponseEntity(jobSummaryList, HttpStatus.OK);
 	}
+
+	@GetMapping("/top")
+	public ResponseEntity getTopRelevantJobs(@RequestHeader("Authorization") String token) {
+		if (!securityService.canAccessRole(token, MemberRole.CLIENT)) {
+			return new ResponseEntity(HttpStatus.UNAUTHORIZED);
+		}
+
+		Member userByToken = securityService.getUserByToken(token);
+
+		List<Ability> userAbilities = this.abilityService.getMemberAbilities(userByToken);
+		List<Job> result = this.jobService.getTopRelevantJobs(userAbilities);
+
+		return new ResponseEntity(result, HttpStatus.OK);
+	}
+
 }
